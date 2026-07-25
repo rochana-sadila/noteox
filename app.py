67a00d664154dbf5b4b2917919c54afaab397309
rochana-sadila <rochanasadila@gmail.com>
2026-07-25 23:04:28 +0530

    Refactor CLI to use subparsers for note and list commands

--- app.py
+++ app.py
@@ -5,15 +5,16 @@ import argparse
 note_path = Path("notes")
 note_path.mkdir(parents=True , exist_ok=True)
 
-parser = argparse.ArgumentParser(description="A Simple Note Taking CLI")
-parser.add_argument(
-        '--note', 
-        action='store_true',
-        help='Make Simple Markdown Note'
-    )
+
+parser = argparse.ArgumentParser(description="Simple Note Taking App")
+subparsers = parser.add_subparsers(dest='command')
+
+note = subparsers.add_parser('note', help='Make markdown note')
+note = subparsers.add_parser('list', help='See avalible note list')
 args = parser.parse_args()
 
-if args.note:
-	print("work")
-else: 
-	print("not work")
+if args.command == 'note':
+    print("working note...")
+
+elif args.command == 'list':
+    print("working list...")
